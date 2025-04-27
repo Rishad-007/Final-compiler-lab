243f26dab1e54f81e885bc00bdff0a09fb75cf9f
Rishad-007 <rishad.nur007@gmail.com>
2025-04-27 22:37:18 +0600

    simple compound complex

--- lex.yy.c
+++ lex.yy.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,10 +361,11 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[19] =
+static const flex_int16_t yy_accept[29] =
     {   0,
-        0,    0,    4,    2,    2,    2,    2,    2,    1,    0,
-        1,    0,    0,    0,    0,    0,    0,    0
+        0,    0,    5,    3,    3,    3,    3,    3,    3,    3,
+        0,    0,    0,    0,    2,    1,    0,    0,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -378,10 +379,10 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    3,    4,    1,    1,
+        1,    1,    1,    1,    1,    1,    3,    4,    5,    6,
 
-        5,    1,    6,    1,    7,    1,    1,    1,    8,    9,
-        1,    1,    1,   10,   11,    1,    1,    1,   12,    1,
+        7,    8,    9,   10,   11,    1,    1,   12,    1,   13,
+       14,    1,    1,   15,   16,   17,   18,    1,   19,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,40 +400,46 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[13] =
+static const YY_CHAR yy_meta[20] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1
+        1,    1,    1,    1,    1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[19] =
+static const flex_int16_t yy_base[30] =
     {   0,
-        0,    0,   28,   29,    5,   22,   15,   13,   29,   20,
-       14,   13,   13,   13,   11,   12,    8,   29
+        0,   17,   42,   43,    0,    0,   33,   25,   25,   28,
+       20,   29,   29,   15,   43,   43,   23,   19,   24,   13,
+       11,    6,    5,    6,    8,    8,    0,   43,    0
     } ;
 
-static const flex_int16_t yy_def[19] =
+static const flex_int16_t yy_def[30] =
     {   0,
-       18,    1,   18,   18,   18,   18,   18,   18,   18,   18,
-       18,   18,   18,   18,   18,   18,   18,    0
+       29,   29,   28,   28,   28,   28,   28,   28,   28,   28,
+       28,   28,   28,   28,   28,   28,   28,   28,   28,   28,
+       28,   28,   28,   28,   28,   28,   28,    0,   28
     } ;
 
-static const flex_int16_t yy_nxt[42] =
+static const flex_int16_t yy_nxt[63] =
     {   0,
-        4,    4,    5,    6,    4,    4,    7,    4,    4,    4,
-        4,    8,    9,    9,   10,   12,    9,   13,   14,   17,
-       15,    9,   16,    9,    9,    9,   11,   18,    3,   18,
-       18,   18,   18,   18,   18,   18,   18,   18,   18,   18,
-       18
+        4,   28,    5,    6,   28,   28,   13,   28,   28,   15,
+        7,   11,   12,    8,   15,    9,   27,   14,   10,    5,
+        6,   26,   25,   24,   23,   15,   22,    7,   21,   20,
+        8,   16,    9,   19,   16,   10,   18,   17,   16,   16,
+       15,   28,    3,   28,   28,   28,   28,   28,   28,   28,
+       28,   28,   28,   28,   28,   28,   28,   28,   28,   28,
+       28,   28
     } ;
 
-static const flex_int16_t yy_chk[42] =
+static const flex_int16_t yy_chk[63] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    5,   17,    5,    8,   16,    8,   11,   15,
-       11,   14,   13,   12,   10,    7,    6,    3,   18,   18,
-       18,   18,   18,   18,   18,   18,   18,   18,   18,   18,
-       18
+       29,    0,    1,    1,    0,    0,    6,    0,    0,   27,
+        1,    5,    5,    1,   26,    1,   25,    6,    1,    2,
+        2,   24,   23,   22,   21,   20,   19,    2,   18,   17,
+        2,   14,    2,   13,   12,    2,   11,   10,    9,    8,
+        7,    3,   28,   28,   28,   28,   28,   28,   28,   28,
+       28,   28,   28,   28,   28,   28,   28,   28,   28,   28,
+       28,   28
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -449,11 +456,13 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "tobeverb.l"
-#line 2 "tobeverb.l"
+#line 1 "simplecompoundcomplex.l"
+#line 2 "simplecompoundcomplex.l"
 #include <stdio.h>
-#line 455 "lex.yy.c"
-#line 456 "lex.yy.c"
+int compound_flag = 0;
+int complex_flag = 0;
+#line 464 "lex.yy.c"
+#line 465 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -670,9 +679,9 @@ YY_DECL
 		}
 
 	{
-#line 5 "tobeverb.l"
+#line 7 "simplecompoundcomplex.l"
 
-#line 675 "lex.yy.c"
+#line 684 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -699,13 +708,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 19 )
+				if ( yy_current_state >= 29 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 29 );
+		while ( yy_base[yy_current_state] != 43 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -731,25 +740,26 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "tobeverb.l"
-{ 
-    printf("To be verb found: %s\n", yytext); 
-}
+#line 8 "simplecompoundcomplex.l"
+{ compound_flag = 1; }
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 9 "tobeverb.l"
-{ 
-    /* Ignore other characters */ 
-}
+#line 9 "simplecompoundcomplex.l"
+{ complex_flag = 1; }
 	YY_BREAK
 case 3:
+/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 12 "tobeverb.l"
+#line 10 "simplecompoundcomplex.l"
+;   // Ignore other characters
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 11 "simplecompoundcomplex.l"
 ECHO;
 	YY_BREAK
-#line 752 "lex.yy.c"
+#line 762 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1046,7 +1056,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 19 )
+			if ( yy_current_state >= 29 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1074,11 +1084,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 19 )
+		if ( yy_current_state >= 29 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 18);
+	yy_is_jam = (yy_current_state == 28);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1754,15 +1764,25 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "tobeverb.l"
+#line 11 "simplecompoundcomplex.l"
+
+
 
 
 int main() {
-    printf("Enter input (Ctrl+D to end):\n");
+    printf("Enter a sentence:\n");
     yylex();
+    
+    if (complex_flag)
+        printf("This is a COMPLEX sentence.\n");
+    else if (compound_flag)
+        printf("This is a COMPOUND sentence.\n");
+    else
+        printf("This is a SIMPLE sentence.\n");
+    
     return 0;
 }
-
 int yywrap() {
     return 1;
 }
+
