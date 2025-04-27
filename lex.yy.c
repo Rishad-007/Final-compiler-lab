55c049847e3cb648742ae6963ea0744092765472
Rishad-007 <rishad.nur007@gmail.com>
2025-04-27 22:09:44 +0600

    word counter

--- lex.yy.c
+++ lex.yy.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,23 +361,23 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[8] =
+static const flex_int16_t yy_accept[11] =
     {   0,
-        0,    0,    4,    2,    1,    1,    0
+        0,    0,    6,    4,    2,    3,    1,    2,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
         1,    1,    1,    1,    4,    4,    4,    4,    4,    4,
         4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
         4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
-        1,    1,    1,    1,    4,    1,    4,    4,    4,    4,
+        1,    1,    1,    1,    1,    1,    4,    4,    4,    4,
 
         4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
         4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
@@ -400,29 +400,31 @@ static const YY_CHAR yy_ec[256] =
 
 static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    1,    2,    2
+        1,    2,    1,    3
     } ;
 
-static const flex_int16_t yy_base[9] =
+static const flex_int16_t yy_base[13] =
     {   0,
-        0,    0,    6,    7,    0,    0,    7,    3
+        0,    0,    7,    8,    0,    8,    0,    0,    0,    8,
+        4,    2
     } ;
 
-static const flex_int16_t yy_def[9] =
+static const flex_int16_t yy_def[13] =
     {   0,
-        7,    1,    7,    7,    8,    8,    0,    7
+       10,    1,   10,   10,   11,   10,   12,   11,   12,    0,
+       10,   10
     } ;
 
-static const flex_int16_t yy_nxt[12] =
+static const flex_int16_t yy_nxt[13] =
     {   0,
-        4,    4,    4,    5,    6,    7,    3,    7,    7,    7,
-        7
+        4,    5,    6,    7,    9,    8,   10,    3,   10,   10,
+       10,   10
     } ;
 
-static const flex_int16_t yy_chk[12] =
+static const flex_int16_t yy_chk[13] =
     {   0,
-        1,    1,    1,    1,    8,    3,    7,    7,    7,    7,
-        7
+        1,    1,    1,    1,   12,   11,    3,   10,   10,   10,
+       10,   10
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -439,14 +441,16 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "identifiercount.l"
-#line 2 "identifiercount.l"
+#line 1 "characterwordcount.l"
+#line 2 "characterwordcount.l"
 #include <stdio.h>
-#include <ctype.h>
 
-int identifier_count = 0;
-#line 448 "lex.yy.c"
-#line 449 "lex.yy.c"
+int char_count = 0;
+int word_count = 0;
+int space_count = 0;
+int line_count = 0;
+#line 452 "lex.yy.c"
+#line 453 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -663,9 +667,9 @@ YY_DECL
 		}
 
 	{
-#line 8 "identifiercount.l"
+#line 10 "characterwordcount.l"
 
-#line 668 "lex.yy.c"
+#line 672 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -692,13 +696,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 11 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 8 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -724,24 +728,42 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 9 "identifiercount.l"
+#line 11 "characterwordcount.l"
 { 
-    identifier_count++; 
-    printf("Identifier found: %s\n", yytext); 
+    word_count++; 
+    char_count += yyleng; 
 }
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 13 "identifiercount.l"
-{ /* Ignore other characters */ }
+#line 15 "characterwordcount.l"
+{ 
+    space_count += yyleng; 
+    char_count += yyleng; 
+}
 	YY_BREAK
 case 3:
+/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 14 "identifiercount.l"
+#line 19 "characterwordcount.l"
+{ 
+    line_count++; 
+    char_count++; 
+}
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 23 "characterwordcount.l"
+{ 
+    char_count++; 
+}
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 26 "characterwordcount.l"
 ECHO;
 	YY_BREAK
-#line 744 "lex.yy.c"
+#line 766 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1038,7 +1060,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 11 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1066,11 +1088,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 11 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 10);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1746,13 +1768,16 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 14 "identifiercount.l"
+#line 26 "characterwordcount.l"
 
 
 int main() {
-    printf("Enter input: ");
+    printf("Enter input (Ctrl+D to end):\n");
     yylex();
-    printf("\nTotal number of identifiers: %d\n", identifier_count);
+    printf("\nCharacter count: %d\n", char_count);
+    printf("Word count: %d\n", word_count);
+    printf("Space count: %d\n", space_count);
+    printf("Line count: %d\n", line_count);
     return 0;
 }
 
