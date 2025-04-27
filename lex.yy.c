dfa91c6e5a35ea441d51c8cd6356bfe8195e0e9e
Rishad-007 <rishad.nur007@gmail.com>
2025-04-27 21:39:44 +0600

    floating point checker

--- lex.yy.c
+++ lex.yy.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,12 +361,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[32] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    6,    4,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    2,    3,    3,    1,
-        0
+        0,    0,    5,    3,    2,    0,    2,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -375,8 +372,8 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    4,    5,
-        5,    5,    6,    5,    5,    7,    5,    1,    1,    1,
+        1,    1,    1,    1,    1,    3,    1,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -401,43 +398,31 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[8] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    1,    2,    2,    2,    2,    2
+        1,    1,    1,    2
     } ;
 
-static const flex_int16_t yy_base[49] =
+static const flex_int16_t yy_base[11] =
     {   0,
-        0,    0,   31,   32,   26,    0,   22,    0,   22,   24,
-        0,   22,    0,   19,    0,    0,    0,    0,    0,    0,
-        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-       32,   22,   21,   20,   19,   18,   17,   16,   15,   14,
-       13,   12,   11,   10,    9,    8,    7,    6
+        0,    0,    8,    9,    2,    0,    0,    0,    9,    5
     } ;
 
-static const flex_int16_t yy_def[49] =
+static const flex_int16_t yy_def[11] =
     {   0,
-       31,    1,   31,   31,   32,   32,   32,   32,   32,   32,
-       33,   32,   34,   32,   35,   36,   37,   38,   39,   40,
-       41,   42,   43,   44,   45,   46,   32,   47,   48,   32,
-        0,   31,   31,   31,   31,   31,   31,   31,   31,   31,
-       31,   31,   31,   31,   31,   31,   31,   31
+        9,    1,    9,    9,    9,   10,    5,   10,    0,    9
     } ;
 
-static const flex_int16_t yy_nxt[40] =
+static const flex_int16_t yy_nxt[14] =
     {   0,
-        4,    4,    5,    6,    6,    6,    7,   30,   29,   28,
-       27,   26,   25,   24,   23,   22,   21,   20,   19,   18,
-       17,   15,   13,    8,   16,   14,   12,   11,   10,    9,
-       31,    3,   31,   31,   31,   31,   31,   31,   31
+        4,    4,    4,    5,    6,    7,    8,    9,    3,    9,
+        9,    9,    9
     } ;
 
-static const flex_int16_t yy_chk[40] =
+static const flex_int16_t yy_chk[14] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,   48,   47,   46,
-       45,   44,   43,   42,   41,   40,   39,   38,   37,   36,
-       35,   34,   33,   32,   14,   12,   10,    9,    7,    5,
-        3,   31,   31,   31,   31,   31,   31,   31,   31
+        1,    1,    1,    1,    5,    5,   10,    3,    9,    9,
+        9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -454,11 +439,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "teletalknumberchecker.l"
-#line 2 "teletalknumberchecker.l"
+#line 1 "floatingnumberchecker.l"
+#line 2 "floatingnumberchecker.l"
 #include <stdio.h>
-#line 460 "lex.yy.c"
-#line 461 "lex.yy.c"
+#line 445 "lex.yy.c"
+#line 446 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -675,9 +660,9 @@ YY_DECL
 		}
 
 	{
-#line 5 "teletalknumberchecker.l"
+#line 5 "floatingnumberchecker.l"
 
-#line 680 "lex.yy.c"
+#line 665 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -704,13 +689,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 32 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 32 );
+		while ( yy_base[yy_current_state] != 9 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -736,31 +721,26 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "teletalknumberchecker.l"
-{ printf("Valid Teletalk number: %s\n", yytext); }
+#line 6 "floatingnumberchecker.l"
+{ printf("Floating point number: %s\n", yytext); }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 7 "teletalknumberchecker.l"
-{ printf("Valid Teletalk number: %s\n", yytext); }
+#line 7 "floatingnumberchecker.l"
+{printf("integer number: %s\n",yytext);}
 	YY_BREAK
 case 3:
+/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 8 "teletalknumberchecker.l"
-{ printf("Invalid number: %s\n", yytext); }
-	YY_BREAK
-case 4:
-/* rule 4 can match eol */
-YY_RULE_SETUP
-#line 9 "teletalknumberchecker.l"
+#line 8 "floatingnumberchecker.l"
 { /* Ignore other characters */ }
 	YY_BREAK
-case 5:
+case 4:
 YY_RULE_SETUP
-#line 10 "teletalknumberchecker.l"
+#line 9 "floatingnumberchecker.l"
 ECHO;
 	YY_BREAK
-#line 763 "lex.yy.c"
+#line 743 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1057,7 +1037,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 32 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1085,11 +1065,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 32 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 31);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1765,11 +1745,11 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "teletalknumberchecker.l"
+#line 9 "floatingnumberchecker.l"
 
 
 int main() {
-    printf("Enter a phone number: ");
+    printf("Enter input: ");
     yylex();
     return 0;
 }
